4de2a2536ce0a1212b27f099bc39ca9a5b3b79d4
Will Anderson <WillRAnderson@outlook.com>
2024-07-23 14:06:26 +1000

    db connect functions added to index.js

--- api/common/db.js
+++ api/common/db.js
@@ -5,68 +5,70 @@
 // let client;
 // let database;
 
-// async function connect() {
-//     // try {
-//         if (!client) {
-//             client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//             await client.connect();
-//             database = client.db('UnityFuel'); // DB name
-//             console.log("Connected to MongoDB");
-//         }
-//         return database;
-//     // } catch (error) {
-//     //     console.error("Error connecting to MongoDB:", error);
-//     //     throw error;
-//     // }
-// }
-
-// async function getItems() {
-//     // try {
-//         const db = await connect();
-//         const collection = db.collection('customers'); // collection name
-//         const items = await collection.find().toArray();
-//         return items;
-//     // } catch (error) {
-//     //     console.error("Error fetching items:", error);
-//     //     throw error;
-//     // }
-// }
-
-// module.exports = { getItems };
-const { MongoClient } = require('mongodb');
-
-const uri = process.env.MONGODB_URI;
-let client;
-
-async function connectToDatabase() {
-  if (!client) {
-    try {
-      client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-      await client.connect();
-    } catch (error) {
-      console.error("Error connecting to MongoDB in db.js:", error);
-      throw error;
-    }
-  }
-  return client.db('UnityFuel');
+async function connect() {
+    // try {
+        if (!client) {
+            client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+            await client.connect();
+            database = client.db('UnityFuel'); // DB name
+            console.log("Connected to MongoDB");
+        }
+        return database;
+    // } catch (error) {
+    //     console.error("Error connecting to MongoDB:", error);
+    //     throw error;
+    // }
 }
 
 async function getItems() {
-  try {
-    const db = await connectToDatabase();
-    if (!db) {
-      throw new Error("Database connection is not established.");
-    }
-    const collection = db.collection('customers');
-    if (!collection) {
-      throw new Error("Collection not found.");
-    }
-    const items = await collection.find({}).toArray();
-    return items;
-  } catch (error) {
-    console.error("Error fetching items:", error);
-    throw error;
-  }
+    // try {
+        const db = await connect();
+        const collection = db.collection('customers'); // collection name
+        const items = await collection.find().toArray();
+        return items;
+    // } catch (error) {
+    //     console.error("Error fetching items:", error);
+    //     throw error;
+    // }
 }
 
 module.exports = { getItems };
+
+
+// const { MongoClient } = require('mongodb');
+
+// const uri = process.env.MONGODB_URI;
+// let client;
+
+// async function connectToDatabase() {
+//   if (!client) {
+//     try {
+//       client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//       await client.connect();
+//     } catch (error) {
+//       console.error("Error connecting to MongoDB in db.js:", error);
+//       throw error;
+//     }
+//   }
+//   return client.db('UnityFuel');
+// }
+
+// async function getItems() {
+//   try {
+//     const db = await connectToDatabase();
+//     if (!db) {
+//       throw new Error("Database connection is not established.");
+//     }
+//     const collection = db.collection('customers');
+//     if (!collection) {
+//       throw new Error("Collection not found.");
+//     }
+//     const items = await collection.find({}).toArray();
+//     return items;
+//   } catch (error) {
+//     console.error("Error fetching items:", error);
+//     throw error;
+//   }
+// }
+
+// module.exports = { getItems };
--- api/message/index.js
+++ api/message/index.js
@@ -31,10 +31,45 @@
 //     }
 // };
 
-const { getItems } = require('../common/db.js');
+// const { getItems } = require('../common/db.js');
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+let client;
+let database;
+
+async function connect() {
+    // try {
+        if (!client) {
+            client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+            await client.connect();
+            database = client.db('UnityFuel'); // DB name
+            console.log("Connected to MongoDB");
+        }
+        return database;
+    // } catch (error) {
+    //     console.error("Error connecting to MongoDB:", error);
+    //     throw error;
+    // }
+}
+
+
+async function getItems() {
+    // try {
+        const db = await connect();
+        const collection = db.collection('customers'); // collection name
+        const items = await collection.find().toArray();
+        return items;
+    // } catch (error) {
+    //     console.error("Error fetching items:", error);
+    //     throw error;
+    // }
+}
 
 module.exports = async function (context, req) {
     try {
+
+
         const items = await getItems();
         context.res = {
             // status: 200, /* Defaults to 200 */
