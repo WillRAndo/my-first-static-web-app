44fa3bd0dedc34ec1e32499781170169f7bd79a0
Will Anderson <WillRAnderson@outlook.com>
2024-07-23 10:19:22 +1000

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -15,43 +15,43 @@
 
 
 // Outputs Text
-// module.exports = async function (context, req) {
-//     context.res.json({
-//         text: "Hello from the API wando"
-//     });
-// };
-
-const data = require('../../data'); 
-require('dotenv').config();
-
-// requests data from UnityFuel Customers
-const { getItems } = require('../../db');
-
 module.exports = async function (context, req) {
-
-    const message = "Hello, World! This is a test endpoint.";
-
-
-    // try {
-        // const items = await getItems();
-
-
-        context.res = {
-            status: 200, /* Defaults to 200 */
-            // body: data,
-            body: {message},
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
-        console.log("API Response:", context.res.body);
-
-
-    // } catch (error) {
-    //     console.error("API Error:", error);
-    //     context.res = {
-    //         status: 500,
-    //         body: "Error connecting to MongoDB: " + error.message
-    //     };
-    // }
+    context.res.json({
+        text: "Hello from the API wando"
+    });
 };
+
+// const data = require('../../data'); 
+// require('dotenv').config();
+
+// // requests data from UnityFuel Customers
+// const { getItems } = require('../../db');
+
+// module.exports = async function (context, req) {
+
+//     const message = "Hello, World! This is a test endpoint.";
+
+
+//     // try {
+//         // const items = await getItems();
+
+
+//         context.res = {
+//             status: 200, /* Defaults to 200 */
+//             // body: data,
+//             body: {message},
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         };
+//         console.log("API Response:", context.res.body);
+
+
+//     // } catch (error) {
+//     //     console.error("API Error:", error);
+//     //     context.res = {
+//     //         status: 500,
+//     //         body: "Error connecting to MongoDB: " + error.message
+//     //     };
+//     // }
+// };
