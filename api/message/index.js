63a8598e2554941df6ef5709814da628108f9f08
Will Anderson <WillRAnderson@outlook.com>
2024-07-23 11:52:30 +1000

    common test

--- api/message/index.js
+++ api/message/index.js
@@ -16,79 +16,38 @@
 
 
 
-const data = require('./data.js'); 
+const data = require('../common/data.js'); 
 
-// const data = {
-//     "_id": "61dbe610755daebc5155e468",
-//     "customerName": "APCO",
-//     "customerContact": null,
-//     "externalData": {
-//         "customerId": 400081
-//     },
-//     "isActive": true,
-//     "eligibleProducts": [
-//         "AUTOGAS"
-//     ],
-//     "__v": 0,
-//     "notifications": {
-//         "email": "saraht@apco.com.au"
-//     }
-// }; 
 
 // Outputs Text
 module.exports = async function (context, req) {
 
     // context.res.json({
     //     text: "Hello from the API wando"
-
     // });
+
     context.res = {
         body: data,
     };
     context.done();
 
-    // context.res.body = {
-    //     status: 200,
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     text: JSON.stringify(data)
-
-    // };
 
 };
 
-// const data = require('../../data'); 
-// require('dotenv').config();
-
-// // requests data from UnityFuel Customers
-// const { getItems } = require('../../db');
+// api/message.js
+// const { getItem } = require('./db');
 
 // module.exports = async function (context, req) {
-
-//     const message = "Hello, World! This is a test endpoint.";
-
-
-//     // try {
-//         // const items = await getItems();
-
-
+//     try {
+//         const item = await getItem();
 //         context.res = {
-//             status: 200, /* Defaults to 200 */
-//             // body: data,
-//             body: {message},
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
+//             // status: 200, /* Defaults to 200 */
+//             body: { text: item ? item.text : "No data found" }
 //         };
-//         console.log("API Response:", context.res.body);
-
-
-//     // } catch (error) {
-//     //     console.error("API Error:", error);
-//     //     context.res = {
-//     //         status: 500,
-//     //         body: "Error connecting to MongoDB: " + error.message
-//     //     };
-//     // }
+//     } catch (error) {
+//         context.res = {
+//             status: 500,
+//             body: "Error connecting to MongoDB: " + error.message
+//         };
+//     }
 // };
